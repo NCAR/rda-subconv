03701a261c29b7154d07b8ee0e43485cc09aba6f
rda-dattore <dattore@ucar.edu>
2025-11-06 13:50:11 -0700

    update server name

--- src/libsubconv/create_download_scripts.cpp
+++ src/libsubconv/create_download_scripts.cpp
@@ -10,26 +10,26 @@ namespace subconv {
 
 void create_download_scripts(const vector<string>& wget_list, string
     dsrqst_root) {
-  putenv(const_cast<char *>("SERVER_NAME=rda.ucar.edu"));
+  putenv(const_cast<char *>("SERVER_NAME=gdex.ucar.edu"));
   auto web_download_directory = args.download_directory;
   replace_all(web_download_directory, dsrqst_root, "");
   if (request_values.ancillary.tarflag != "Y") {
     std::ofstream download_script_fs((args.download_directory + "/unix-wget." +
         args.rqst_index + ".csh").c_str());
-    create_wget_script(wget_list, "https://request.rda.ucar.edu",
+    create_wget_script(wget_list, "https://request.gdex.ucar.edu",
         web_download_directory, "csh", &download_script_fs);
     download_script_fs.close();
     download_script_fs.clear();
     download_script_fs.open((args.download_directory + "/unix-curl." + args.
         rqst_index + ".csh").c_str());
-    create_curl_script(wget_list, "https://request.rda.ucar.edu",
-        web_download_directory,"csh",&download_script_fs);
+    create_curl_script(wget_list, "https://request.gdex.ucar.edu",
+        web_download_directory, "csh", &download_script_fs);
     download_script_fs.close();
     download_script_fs.clear();
     download_script_fs.open((args.download_directory + "/dos-wget." + args.
         rqst_index + ".bat").c_str());
-    create_wget_script(wget_list, "https://request.rda.ucar.edu",
-        web_download_directory,"bat",&download_script_fs);
+    create_wget_script(wget_list, "https://request.gdex.ucar.edu",
+        web_download_directory, "bat", &download_script_fs);
     download_script_fs.close();
     download_script_fs.clear();
   }
